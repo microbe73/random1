238ead079ec4bfe714f0b20ac411820642a39989
rit roy <rayatrr@gmail.com>
2024-06-27 23:54:01 -0400

    map function in forth with the power of ', loops, and writing down the stack after each operation on a post-it note

--- forth_stuff/forth_arrays.fs
+++ forth_stuff/forth_arrays.fs
@@ -1,4 +1,9 @@
 ( so the plan is to define words for arrays stored in the following way:
 len, [0, 1, 2, ...]. I think this is the best way to do like map or filter
-or reduce )
+or reduce. the ' [word] operator gives the address of a word and then
+execute executes it )
 : square dup * ;
+: map ( op arr -- arr )
+  dup @ 0 u+do
+  1 cells + dup @ rot tuck execute rot dup -rot !
+  loop ;
