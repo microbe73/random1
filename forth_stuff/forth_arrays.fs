c2b8eaa2d265b1ab282bf829765a3d605d285f40
rit roy <rayatrr@gmail.com>
2024-07-01 16:39:36 -0400

    completed reduce with even more post-its to write out the stack after each operation

--- forth_stuff/forth_arrays.fs
+++ forth_stuff/forth_arrays.fs
@@ -3,7 +3,13 @@ len, [0, 1, 2, ...]. I think this is the best way to do like map or filter
 or reduce. the ' [word] operator gives the address of a word and then
 execute executes it )
 : square dup * ;
+
 : map ( op arr -- arr )
   dup @ 0 u+do
   1 cells + dup @ rot tuck execute rot dup -rot !
+  loop ;
+
+: reduce ( op arr -- arr )
+  dup @ swap 1 cells + dup @ rot 1 u+do
+  swap 1 cells + dup @ 2swap -rot 2tuck execute 2swap nip -rot
   loop ;
