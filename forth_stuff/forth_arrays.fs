579551125b06683714c833097a4fb710c8dd65e6
rit roy <rayatrr@gmail.com>
2024-07-02 16:36:30 -0400

    halfway to a filter function, can now count all of the elements of an array that meet a condition. Require more post-its

--- forth_stuff/forth_arrays.fs
+++ forth_stuff/forth_arrays.fs
@@ -4,12 +4,26 @@ or reduce. the ' [word] operator gives the address of a word and then
 execute executes it )
 : square dup * ;
 
-: map ( op arr -- arr )
+: map ( op arr -- op arr )
   dup @ 0 u+do
   1 cells + dup @ rot tuck execute rot dup -rot !
   loop ;
 
-: reduce ( op arr -- arr )
+: reduce ( op arr -- op arr )
   dup @ swap 1 cells + dup @ rot 1 u+do
   swap 1 cells + dup @ 2swap -rot 2tuck execute 2swap nip -rot
   loop ;
+
+: positive
+  0 > ;
+
+: ccount ( op arr -- op arr num [counts the number of elements in an array that meet a condition] )
+  0 swap dup @ 0 u+do
+  1 cells + dup @ -rot 2swap swap tuck execute 
+  if
+    rot 1 + rot
+  else
+    -rot
+  endif
+  loop swap;
+
