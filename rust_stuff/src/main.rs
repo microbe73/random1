1a5d2cfde3f714f5e58474f773134bf9e8d7a36e
rit roy <rayatrr@gmail.com>
2024-12-18 13:34:44 -0500

    learning a bit more rust

--- rust_stuff/src/main.rs
+++ rust_stuff/src/main.rs
@@ -1,9 +1,47 @@
+use std::collections::HashMap;
 fn main() {
-    let x = 5;
-    let y = x + 1;
-    {
-        let x = y * 2;
-        println!("The value of x in the inner scope is: {x}");
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    let _field_name = String::from("Favorite color");
+    let _field_value = String::from("Blue");
+
+    let mut hmap = HashMap::new();
+    let mut nums = vec![5, 2, 6, 81, 2, 4, 9];
+    nums.sort();
+    println!("{nums:?}");
+    let length = nums.len();
+    if length % 2 == 0 {
+        let med = (&nums[(length / 2) - 1] + nums[length / 2]) / 2;
+        println!("{med}");
     }
-    println!("The value of x is: {y}");
+    if length % 2 == 1 {
+        let med = &nums[(length - 1) / 2];
+        println!("Median: {med}");
+    }
+    for num in &nums {
+        let key = num.to_string();
+        let val = hmap.get(&key).copied();
+        match val {
+            Some(n) => hmap.insert(key, n + 1),
+            None => hmap.insert(key, 1),
+        };
+    }
+    let mut max = 0;
+    let mut mode = 0;
+    for num in &nums {
+        let key = num.to_string();
+        let val = hmap.get(&key).copied();
+        match val {
+            Some(n) => {
+                if n > max {
+                    mode = *num;
+                    max = n;
+                }
+            }
+            None => max = 0,
+        }
+    }
+    println!("Mode: {mode}");
 }
