5cbcaec86137c14570e96fb12d9066e51ab8e802
rit roy <rayatrr@gmail.com>
2025-02-11 01:08:32 -0600

    undo feature coming along in ed, along with me messing around with floating point arithmetic in Forth

--- forth_stuff/ed.fs
+++ forth_stuff/ed.fs
@@ -1,28 +1,37 @@
 ( Editor Variables )
-variable fh
+variable fh ( file handler )
 variable curr-line ( struct pointer, @ needed to get struct  )
-variable num-lines
-variable fhead
+variable num-lines ( int pointer )
+variable fhead ( struct pointer )
 variable initial-mem
+variable last-edit ( struct pointer )
 256 Constant max-line
 create line-buffer max-line allot
-create buffer2 max-line allot
+
 begin-structure linelist ( -- u )
-        field: linelist-next ( intlist -- addr1 ) ( struct pointer )
-        field: linelist-line  ( intlist -- addr2 ) ( string pointer [char*[]] )
-        field: linelist-len ( intlist -- addr3 ) ( int pointer )
+    field: linelist-next ( intlist -- addr1 ) ( struct pointer )
+    field: linelist-line  ( intlist -- addr2 ) ( string pointer [char*[]] )
+    field: linelist-len ( intlist -- addr3 ) ( int pointer )
 end-structure
-( I think i'm pretty happy with the state of the editor for right now, it has pretty much all the basic features I would want with inserting, deleting,
-copying, search and replace etc. and I'm actually confident that it all works. I may implement the bounds checking but it's not too serious and most of the
-time GForth will just throw an out of bounds error anyway )
+( The next step: undo feature. The way the editor works right now this is actually "free" in a memory sense, since we are re-assigning memory a bunch already
+ also it is somewhat clear to me how to do it )
+
+( I think by induction these structures only need to assume they were the most recent edit so line number stuff shouldnt be an issue )
+begin-structure undonode
+    field: undonode-edittype ( int* )
+    field: undonode-prevedit ( undonode* )
+    field: undonode-linenum ( undoin -- num ) ( int* )
+    field: undonode-textptr ( char** )
+    field: undonode-len ( int* )
+end-structure
+
 
 
 : bfill   max-line 0 u+do line-buffer i + 0 swap ! loop ;
-: b2fill  max-line 0 u+do buffer2 i + 0 swap ! loop ;
 bfill
-
 0 num-lines !
 
+( QOL WORDS )
 ( []  -- linelist-line [ gets pointer to current line's text ] )
 : gcline  curr-line @ linelist-line ;
 : gclen   curr-line @ linelist-len ;
@@ -38,23 +47,9 @@ bfill
 ( line-num -- [sets curr-line to the addressed line] )
 : addify  rcurr 1 u+do n-line loop ;
 
-( addr u -- [Opens the file] )
-: open    r/w open-file throw fh ! ;
-: close   fh @ close-file throw ;
-: clean   0 0 fh @ reposition-file throw ;
-: lat     linelist allocate throw ;
-: l-load  { len } align here line-buffer align here len allot len cmove gcline ! len gclen ! ( set values for this line )
-          lat gcnext ! gcnext @ curr-line !  nullify ( move pointer to next line, set to 0s to avoid weird bugs ) ;
-
-( ptr -- [increase the value pointed to by 1] )
+( ptr -- [increase/decrease the value pointed to by 1] )
 : ++      dup @ 1 + swap ! ;
-
-: f-load  begin bfill line-buffer max-line fh @ read-line throw num-lines ++ while 1 + l-load repeat bfill ;
-( c-addr u -- [load a file into memory] )
-: init    open lat fhead ! rcurr clean f-load ;
-
-: deinit  rcurr begin curr-line @ dup linelist-next @ swap free throw dup curr-line ! until ;
-
+: --      dup @ 1 - swap ! ;
 : gl 1 num-lines @ 1 - ;
 
 : itoa     48 + emit ;
@@ -63,62 +58,94 @@ bfill
 ( num -- [prints out the number to stdout] )
 : itos     dup get-dig 0 u+do dup get-plc 2dup / itoa mod loop drop ;
 
+
+
+( FILE I/O OPERATIONS )
+( addr u -- [Opens the file] )
+: open    r/w open-file throw fh ! ;
+
+: close   fh @ close-file throw ;
+
+: clean   0 0 fh @ reposition-file throw ;
+
+: lat     linelist allocate throw ;
+
+: l-load  { len } align here line-buffer align here len allot len cmove gcline ! len gclen ! ( set values for this line )
+          lat gcnext ! gcnext @ curr-line !  nullify ( move pointer to next line, set to 0s to avoid weird bugs ) ;
+
+: f-load  begin bfill line-buffer max-line fh @ read-line throw num-lines ++ while 1 + l-load repeat bfill ;
+
+( c-addr u -- [load a file into memory] )
+: init    open lat fhead ! rcurr clean f-load ;
+
+: deinit  rcurr begin curr-line @ dup linelist-next @ swap free throw dup curr-line ! until ;
+
+
+
 ( start-line end-line )
 : p        10 emit over addify 1 + swap u+do gcline @ i itos 58 emit 32 emit gclen @ type 10 emit n-line loop ;
 ( print entire file to stdout )
 : ,p       gl p ;
 
-( : in      bfill addify gcline -rot line-buffer swap dup 1 + { len } cmove
-          here line-buffer here len allot len cmove swap !
-          len gclen ! ; )
-
-( c-addr u -- new-addr [move an input string into a new address, null-terminate it] )
-: mhere  { len } here len allot tuck len cmove 0 c, ;
-( c-addr u line-num -- ) ( replace line with other text )
-: in     addify { len } len mhere gcline ! len 1 + gclen ! ;
-
-( c-addr u line-num -- ) ( insert text between line-num and line-num + 1, or at the end if it is the last line )
-( : ia      bfill -rot line-buffer swap dup 1 + { len } cmove addify curr-line @ linelist-next @ )
-          ( linelist allocate throw dup curr-line @ linelist-next ! swap over linelist-next !  )
-( sets the current line's next line to the new one and the new line's next line to the one after )
-         ( line-buffer here len allot tuck len cmove swap linelist-line ! ) ( sets the new line's string pointer to the text )
-         ( n-line len gclen ! ) ( sets the new line's int pointer to the length of the new line )
-          ( num-lines @ 1 + num-lines ! ; )
-
-: ia     addify { len } len mhere lat gcnext @ over gcnext ! over linelist-next ! len 1 + over
-          linelist-len ! linelist-line ! num-lines ++ ;
-( c-addr u ) ( insert text before the first line )
-( : i0      bfill line-buffer swap dup 1 + { len } cmove fhead @
-          linelist allocate throw dup fhead ! linelist-next !
-          line-buffer here max-line allot tuck max-line cmove fhead @ linelist-line !
-          len fhead @ linelist-len ! 
-          num-lines @ 1 + num-lines ! ; )
-: i0     { len } len mhere lat tuck linelist-line ! fhead @ over linelist-next !
-          len 1 + over linelist-len ! fhead ! num-lines ++ ;
-( c-addr u line-num c-num -- ) ( insert text at a specific character in a line )
-: ic      bfill { len line-num c-num } len line-num addify gclen @ c-num < if -1 abort" Illegal insertion (beyond end of line)" endif
-          gcline @ line-buffer c-num cmove line-buffer c-num + swap cmove gcline @ c-num + line-buffer len c-num + + gclen @ c-num - cmove 
-    ( move everything into the line buffer )
-          here line-buffer here len gclen @ + allot len gclen @ + cmove gcline !
-          len gclen @ + gclen ! ;
+( INSERTION WORDS SETUP )
 ( c-num -- c-num{!} [checks if c-num is more than the length of the current line, aborts if it is] )
-: bound   gclen @ over < if -1 abort" Illegal insertion beyond end of line" endif ;
+: bound     gclen @ over < if -1 abort" Illegal insertion beyond end of line" endif ;
 ( c-num -- c-num [moves the first c characters of the current line into the line buffer] )
-: prein   gcline @ over line-buffer swap cmove ;
+: prein     gcline @ over line-buffer swap cmove ;
 ( c-addr u c-num -- u c-num [moves the u characters offset by c places into the line buffer] )
-: newin   dup >r line-buffer + swap dup >r cmove r> r> ;
+: newin     dup >r line-buffer + swap dup >r cmove r> r> ;
 ( u c-num -- new-len [inserts the last part into the line buffer] )
-: postin  { u c-num } gcline @ c-num + line-buffer c-num u + + 
-          gclen @ c-num - cmove gclen @ u + ;
+: postin    { u c-num } gcline @ c-num + line-buffer c-num u + + 
+            gclen @ c-num - cmove gclen @ u + ;
+( c-addr u -- new-addr [move an input string into a new address, null-terminate it] )
+: mhere     { len } here len allot tuck len cmove 0 c, ;
+: unat      undonode allocate throw ;
+( lnum n -- [creates a new undonode, adds it to list] )
+: usetup    unat tuck undonode-edittype ! gcline @ over undonode-textptr ! gclen @ over undonode-len !
+            over over undonode-linenum ! nip last-edit @ over undonode-prevedit ! last-edit ! ;
+( INSERTION WORDS )
+( c-addr u line-num -- ) ( replace line with other text )
+: in        dup addify 3 usetup { len } len mhere gcline ! len 1 + gclen ! ;
+
+: ia        dup addify 4 usetup { len } len mhere lat gcnext @ over gcnext ! over linelist-next ! len 1 + over
+            linelist-len ! linelist-line ! num-lines ++ ;
+
+( c-addr u ) ( insert text before the first line )
+: i0        rcurr 1 1 usetup { len } len mhere lat tuck linelist-line ! fhead @ over linelist-next !
+            len 1 + over linelist-len ! fhead ! num-lines ++ ;
+
 ( c-addr u line-num c-num -- ) ( insert text at a specific character in a line )
-: ic2     bfill swap dup >r addify bound prein newin postin line-buffer swap r> in ;
-: --      dup @ 1 - swap ! ;
-( line-num ) ( delete given line )
-: del     1 - addify curr-line @ linelist-next @ curr-line @ linelist-next @ linelist-next @ curr-line @ linelist-next !
-          free throw num-lines @ 1 - num-lines ! ;
-: del2     1 - addify gcnext @ gcnext @ linelist-next @ gcnext ! free throw num-lines -- ;
+: ic        bfill swap dup >r addify bound prein newin postin line-buffer swap 1 - r> .s in ;
+
+( DELETION WORDS )
+( line-num -- )
+: del       dup dup addify 5 usetup 1 - addify gcnext @ gcnext @ linelist-next @ gcnext ! free throw num-lines -- ;
 ( delete the first line )
-: d0      fhead @ fhead @ linelist-next @ fhead ! free throw num-lines -- ;
+: d0        rcurr 1 2 usetup fhead @ fhead @ linelist-next @ fhead ! free throw num-lines -- ;
+
+( UNDO WORDS )
+
+: undid     last-edit @ undonode-prevedit last-edit ! ;
+( undonode -- )
+: uni0      d0 ;
+: und0      lat over undonode-textptr @ over linelist-line ! over undonode-len @ over linelist-len ! \ same as dl just with fhead instead
+            dup linelist-next fhead @ ! fhead ! drop num-lines ++ ; \ set the new line to be in fhead, set its next line to be current fhead
+: unin      dup undonode-linenum @ addify dup undonode-textptr @ gcline ! undonode-len gclen ! ;
+: unia      undonode-linenum @ 1 + del ;
+: undl      dup undonode-linenum @ 1 - addify lat over undonode-textptr @ over linelist-line ! \ allocate new linelist, set text to be stored, also addify prev line
+            over undonode-len @ over linelist-len ! gcnext @ over linelist-next ! gcnext ! drop num-lines ++ ;
+( : unsr    unin )
+( : unic    unin )
+( undonode -- )
+: undo      dup dup undonode-edittype @
+            case
+                1 of uni0 undid endof
+                2 of und0 undid endof
+                3 of unin undid endof
+                4 of unia undid endof
+                5 of undl undid endof
+                drop s" didn't undo anything might be error but the experienced programmer will know what's wrong" type
+            endcase ;
 
 create newl 1 allot
 10 newl !
@@ -130,25 +157,18 @@ create newl 1 allot
 ( line-num -- c-addr len ) ( copy a line )
 : c       addify gcline @ gclen @ ;
 
-
+( SEARCHING AND REPLACING )
 : no-mat  2dup gcline @ + c@ swap line-buffer + c! 1 + swap 1 + swap ;
-( c-addr1 u1 c-addr2 u2 line-num -- [replace string 2 with string 1 on line-num] )
-: sr      b2fill dup addify gclen @ { c1 u1 c2 u2 line-num length }
-          0 0 begin gcline @ over + u2 c2 u2 compare
-          if 2dup gcline @ + c@ swap buffer2 + c! 1 + swap 1 + swap ( not a match )
-          else over buffer2 + c1 swap u1 cmove u2 + swap u1 + swap gclen @ u1 u2 - + gclen ! endif ( match between the strings )
-          dup length 1 - > until drop drop ; ( this is criminal forth style here but it works so wtv )
 
-: sr2     bfill dup addify gclen @ { c1 u1 c2 u2 line-num length }
-          0 0 begin gcline @ over + u2 c2 u2 compare if no-mat 
+( c-addr1 u1 c-addr2 u2 line-num -- [replace string 2 with string 1 on line-num] )
+: sr     bfill dup addify gclen @ { c1 u1 c2 u2 line-num length }
+          0 0 begin gcline @ over + u2 c2 u2 compare if no-mat
           else over line-buffer + c1 swap u1 cmove u2 + swap u1 + swap gclen @ u1 u2 - + gclen ! endif
           dup length 1 - > until drop drop ;
-( some things can't actually be refactored yet, maybe old me was just cooking with these, but tbh I see maybe why
-locals could be bad Forth practice they make it hard to do "the forth thing" where you hyper-modularize because your
-program loses segmentation )
+
          
 ( c-addr1 u1 c-addr2 u2 start-line end-line -- [replace string 2 with string 1 on all lines in a range] )
-: srmul   { c1 u1 c2 u2 l1 l2 } l2 1 + l1 u+do c1 u1 c2 u2 i sr2 line-buffer gclen @ 1 - i in loop ;
+: srmul   { c1 u1 c2 u2 l1 l2 } l2 1 + l1 u+do c1 u1 c2 u2 i sr line-buffer gclen @ 1 - i in loop ;
 : sc      { c1 u1 line-num } 0 line-num addify begin gcline @ over + u1 c1 u1 compare
           0= if line-num . dup . 59 emit endif 1 + dup gclen @ 1 - >= until drop ;
 
--- forth_stuff/fp.fs
+++ forth_stuff/fp.fs
@@ -1,5 +1,5 @@
 : qform ( F: a b c -- x- x+ )
-frot ftuck 4e0 f* f* frot ftuck -1e0 f* frot  frot fswap fdup f* fswap f- fsqrt fover fover f+ frot frot f- frot 2e0 f* ftuck f/ frot frot f/
+frot ftuck 4e0 f* f* frot ftuck -1e0 f* frot  frot fswap fdup f* fswap f- fsqrt fover fover f+ frot frot f- frot 2e0 f* ftuck f/ frot  frot f/
 ;
 : discr ( F: a b c -- discriminant ) fswap fdup f* frot frot 4e0 f* f* f- fsqrt ;
 : qflocal ( F: a b c -- x- x+ )
